3ec00619228ff9eecf616f53f6eca4dcdc1a516a
John Quarmor <johnequarmor@gmail.com>
2025-05-07 02:08:40 +0000

    Added script to throw error message if directory path isn't passed.

--- explorer.js
+++ explorer.js
@@ -2,11 +2,14 @@ const fs = require("fs");
 const path = require("path");
 
 function getDirectoryContents(dirPath) {
+	if (!dirPath) throw new Error("Path is undefined");
+
 	const contents = fs.readdirSync(dirPath).map((name) => {
 		const fullPath = path.join(dirPath, name);
 		const isDirectory = fs.lstatSync(fullPath).isDirectory();
 		return { name, fullPath, isDirectory };
 	});
+
 	return contents;
 }
 
